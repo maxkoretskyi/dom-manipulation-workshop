1d82ec385679ecfc718276e590c5d888b55cec98
maksym.koretskyi <maksym.koretskiy@globallogic.com>
2018-03-29 11:02:37 +0300

    Custom NgIf directive (optimized)

--- src/app/app-ng-if.directive.ts
+++ src/app/app-ng-if.directive.ts
@@ -1,19 +1,21 @@
-import { Directive, Input, OnChanges, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, Input, OnChanges, TemplateRef, ViewContainerRef, ViewRef } from '@angular/core';
 
 @Directive({
   selector: '[appNgIfDirective]'
 })
 export class AppNgIfDirective implements OnChanges {
   @Input() appNgIfDirective;
+  view: ViewRef;
 
   constructor(private vc: ViewContainerRef, private template: TemplateRef<any>) {
+    this.view = this.template.createEmbeddedView(null);
   }
 
   ngOnChanges() {
     if (this.appNgIfDirective) {
-      this.vc.createEmbeddedView(this.template);
+      this.vc.insert(this.view);
     } else {
-      this.vc.remove();
+      this.vc.detach();
     }
   }
 }
