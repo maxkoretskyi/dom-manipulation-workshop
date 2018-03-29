9d407870e38fdaad9d52df9ff72ecb1a35ce2ad8
maksym.koretskyi <maksym.koretskiy@globallogic.com>
2018-03-29 09:53:54 +0300

    Removing child component from DOM using VC (logic inside a component)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BComponent } from './b-component';
 import { AComponent } from './a-component';
-import { RemoveElementDirective } from './remove-element.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     AComponent,
-    BComponent,
-    RemoveElementDirective
+    BComponent
   ],
   imports: [
     BrowserModule
