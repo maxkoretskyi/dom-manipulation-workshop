c11843d2ad36dabc43464bfb0a5f34221715b9e4
maksym.koretskyi <maksym.koretskiy@globallogic.com>
2018-04-01 16:11:15 +0300

    Solution 3a: implement custom ngIf directive

--- src/app/aid-ng-if.directive.ts
+++ src/app/aid-ng-if.directive.ts
@@ -1,8 +1,19 @@
-import { Directive, Input } from '@angular/core';
+import { Directive, Input, OnChanges, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[aidNgIf]'
 })
-export class AiDNgIfDirective {
+export class AiDNgIfDirective implements OnChanges {
   @Input() aidNgIf;
+
+  constructor(private vc: ViewContainerRef, private template: TemplateRef<any>) {
+  }
+
+  ngOnChanges() {
+    if (this.aidNgIf) {
+      this.vc.createEmbeddedView(this.template);
+    } else {
+      this.vc.remove();
+    }
+  }
 }
