a19c0851b38d5e072c153922e460abbf287ac284
maksym.koretskyi <maksym.koretskiy@globallogic.com>
2018-04-01 16:32:25 +0300

    Solution 4: render a component dynamically

--- src/app/aid-render-dynamic-component.directive.ts
+++ src/app/aid-render-dynamic-component.directive.ts
@@ -1,8 +1,19 @@
-import { Directive, Input } from '@angular/core';
+import { ComponentFactoryResolver, Directive, Input, OnChanges, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[aidRenderDynamicComponent]'
 })
-export class AiDRenderDynamicComponentDirective {
+export class AiDRenderDynamicComponentDirective implements OnChanges {
   @Input() aidRenderDynamicComponent;
+
+  constructor(private vc: ViewContainerRef, private resolver: ComponentFactoryResolver) {
+  }
+
+  ngOnChanges() {
+    if (this.aidRenderDynamicComponent) {
+      this.vc.clear();
+      const factory = this.resolver.resolveComponentFactory(this.aidRenderDynamicComponent);
+      this.vc.createComponent(factory);
+    }
+  }
 }
