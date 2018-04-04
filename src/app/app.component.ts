f7859518f7f597fb45a77888ee6ab6a0016ff3ce
maksym.koretskyi <maksym.koretskiy@globallogic.com>
2018-04-04 09:27:49 +0300

    Workshop starting commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,10 @@
-import { Component, OnInit, TemplateRef, ViewChild, ViewContainerRef, ViewRef } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'aid-root',
-  template: `
-    <div class="top">
-      <div>top</div>
-      <ng-container #top></ng-container>
-    </div>
-    <div class="bottom">
-      <div>bottom</div>
-      <ng-container #bottom></ng-container>
-    </div>
-    <ng-template>
-      <aid-a-comp></aid-a-comp>
-    </ng-template>
-    <button (click)="move()">move</button>
-  `,
-  styles: ['.top, .bottom {border: 1px solid black; padding: 10px; margin: 10px 0; height: 30px; }']
+  template: `Welcome to the Advanced DOM manipulation scenarios workshop`,
 })
-export class AiDComponent implements OnInit {
-  @ViewChild(TemplateRef) t;
-  @ViewChild('top', {read: ViewContainerRef}) top;
-  @ViewChild('bottom', {read: ViewContainerRef}) bottom;
-
-  ngOnInit() {
-    this.top.createEmbeddedView(this.t);
-  }
-
-  move() {
-    if (this.top.length === 0) {
-      const viewRef = this.bottom.detach(0);
-      this.top.insert(viewRef);
-    } else {
-      const viewRef = this.top.detach(0);
-      this.bottom.insert(viewRef);
-    }
-  }
+export class AiDComponent {
 }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AiDComponent } from './app.component';
-import { AiDBComponent } from './aid-b-component';
-import { AiDAComponent } from './aid-a-component';
 
 @NgModule({
   declarations: [
     AiDComponent,
-    AiDAComponent,
-    AiDBComponent
-  ],
-  entryComponents: [
-    AiDAComponent,
-    AiDBComponent
   ],
   imports: [
     BrowserModule
