429cd1d993130d45ff73e1893c13c96f2c8f093f
maksym.koretskyi <maksym.koretskiy@globallogic.com>
2018-04-06 17:12:27 +0300

    Solution 1a (component): change color by adding an attribute

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'aid-root',
   template: `<span #el>I want to be green</span>`,
-  styles: ['[highlight] {color: green}']
+  styles: ['[highlight] {color: green}'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AiDComponent {
+export class AiDComponent implements AfterViewInit {
+  @ViewChild('el') span: ElementRef;
+
+  ngAfterViewInit() {
+    this.span.nativeElement.setAttribute('highlight', '');
+  }
 }
