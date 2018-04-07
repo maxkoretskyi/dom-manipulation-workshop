eea1b4e3714b001e97c357f02f258ed8a43d4161
maksym.koretskyi <maksym.koretskiy@globallogic.com>
2018-04-07 15:42:59 +0300

    Solution 6 (component): Initialize and move a component (host view) between containers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, ComponentRef, Injector, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { AiDAComponent } from './aid-a-component';
 
 @Component({
   selector: 'aid-root',
@@ -11,12 +12,35 @@ import { Component } from '@angular/core';
       <div>bottom</div>
       <ng-container #bottom></ng-container>
     </div>
+    <ng-template>
+      <aid-a-comp></aid-a-comp>
+    </ng-template>
     <button (click)="move()">move</button>
   `,
   styles: ['.top, .bottom {border: 1px solid black; padding: 10px; margin: 10px 0; height: 30px; }']
 })
-export class AiDComponent {
+export class AiDComponent implements OnInit {
+  @ViewChild('top', {read: ViewContainerRef}) top;
+  @ViewChild('bottom', {read: ViewContainerRef}) bottom;
+  aComponentRef: ComponentRef<AiDAComponent>;
+
+  constructor(private resolver: ComponentFactoryResolver, private injector: Injector) {
+  }
+
+  ngOnInit() {
+    const factory = this.resolver.resolveComponentFactory(AiDAComponent);
+    this.aComponentRef = factory.create(this.injector);
+    this.top.insert(this.aComponentRef.hostView);
+  }
+
   move() {
+    if (this.top.length === 0) {
+      this.bottom.detach();
+      this.top.insert(this.aComponentRef.hostView);
+    } else {
+      this.top.detach();
+      this.bottom.insert(this.aComponentRef.hostView);
+    }
   }
 }
 
