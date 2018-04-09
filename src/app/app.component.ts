439e42a346793df5aa4a32b1b3d12ef6a0101b7d
maximus <m.koretskyi@gmail.com>
2018-04-09 12:11:30 +0300

    Workshop starting commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,8 @@
-import { Component, ComponentFactoryResolver, ComponentRef, Injector, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
-import { AiDAComponent } from './aid-a-component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'aid-root',
-  template: `
-    <div class="top">
-      <div>top</div>
-      <ng-container #top></ng-container>
-    </div>
-    <div class="bottom">
-      <div>bottom</div>
-      <ng-container #bottom></ng-container>
-    </div>
-    <ng-template>
-      <aid-a-comp></aid-a-comp>
-    </ng-template>
-    <button (click)="move()">move</button>
-  `,
-  styles: ['.top, .bottom {border: 1px solid black; padding: 10px; margin: 10px 0; height: 30px; }']
+  template: `Welcome to the Advanced DOM manipulation scenarios workshop`,
 })
-export class AiDComponent implements OnInit {
-  @ViewChild('top', {read: ViewContainerRef}) top;
-  @ViewChild('bottom', {read: ViewContainerRef}) bottom;
-  aComponentRef: ComponentRef<AiDAComponent>;
-
-  constructor(private resolver: ComponentFactoryResolver, private injector: Injector) {
-  }
-
-  ngOnInit() {
-    const factory = this.resolver.resolveComponentFactory(AiDAComponent);
-    this.aComponentRef = factory.create(this.injector);
-    this.top.insert(this.aComponentRef.hostView);
-  }
-
-  move() {
-    if (this.top.length === 0) {
-      this.bottom.detach();
-      this.top.insert(this.aComponentRef.hostView);
-    } else {
-      this.top.detach();
-      this.bottom.insert(this.aComponentRef.hostView);
-    }
-  }
+export class AiDComponent {
 }
-
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AiDComponent } from './app.component';
-import { AiDAComponent } from './aid-a-component';
 
 @NgModule({
   declarations: [
     AiDComponent,
-    AiDAComponent
-  ],
-  entryComponents: [
-    AiDAComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
   bootstrap: [AiDComponent]
 })
 export class AppModule {
