da02d5153b80846916b58f26ae35253bb8cd0d96
maximus <m.koretskyi@gmail.com>
2018-04-19 07:39:31 +0300

    Solution 1a (component): change a color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<span #el>I want to be green</span>`,
-  styles: ['[highlight] {color: green}']
+  template: `<span #el>I want to be red</span>`,
+  styles: ['[highlight] {color: red}']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('el') span: ElementRef;
+
+  ngAfterViewInit() {
+    this.span.nativeElement.setAttribute('highlight', '');
+  }
 }
+
