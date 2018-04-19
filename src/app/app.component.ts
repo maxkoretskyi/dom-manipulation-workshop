3ab555eb542876512f278215303074a37bc720be
maximus <m.koretskyi@gmail.com>
2018-04-19 08:30:13 +0300

    Workshop starting commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,9 @@
-import { Component, ComponentFactoryResolver, ComponentRef, Injector, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
-import { AComponent } from './a.component';
-import { BComponent } from './b.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <button (click)="show('a')">Show A component</button>
-    <button (click)="show('b')">Show B component</button>
-    <ng-container #vc></ng-container>
-  `
+  template: `Welcome to the Advanced DOM manipulation scenarios workshop`,
 })
-export class AppComponent implements OnInit {
-  @ViewChild('vc', {read: ViewContainerRef}) vc: ViewContainerRef;
-  aComponentRef: ComponentRef<AComponent>;
-  bComponentRef: ComponentRef<BComponent>;
-
-  constructor(private resolver: ComponentFactoryResolver, private injector: Injector) {
-  }
-
-  ngOnInit() {
-    const aComponentFactory = this.resolver.resolveComponentFactory(AComponent);
-    const bComponentFactory = this.resolver.resolveComponentFactory(BComponent);
-    this.aComponentRef = aComponentFactory.create(this.injector);
-    this.bComponentRef = bComponentFactory.create(this.injector);
-  }
-
-  show(type) {
-    const view = type === 'a' ? this.aComponentRef.hostView : this.bComponentRef.hostView;
-    this.vc.detach();
-    this.vc.insert(view);
-  }
+export class AppComponent {
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { AComponent } from './a.component';
-import { BComponent } from './b.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AComponent,
-    BComponent
-  ],
-  entryComponents: [
-    AComponent,
-    BComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
